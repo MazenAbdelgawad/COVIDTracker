906ffd5ff1458e44263731c9a0569a4e49fc704f
Mazen <eng.mazen96@gmail.com>
2020-04-12 04:04:58 +0200

    Add countryAdapter

--- app/src/main/java/iti/intake40/covidtracker/view/activites/HomeActivity.kt
+++ app/src/main/java/iti/intake40/covidtracker/view/activites/HomeActivity.kt
@@ -2,12 +2,38 @@ package iti.intake40.covidtracker.view.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import iti.intake40.covidtracker.R
+import iti.intake40.covidtracker.model.Country
+import iti.intake40.covidtracker.view.adapters.HomeAdapter
+import iti.intake40.covidtracker.viewmodel.CountriesViewModel
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+    private var countriesViewModel: CountriesViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        countriesViewModel = ViewModelProviders.of(this).get(CountriesViewModel::class.java)
+
+        countriesViewModel?.refreshCountriesFromApi()
+
+        countriesViewModel?.getCountries()?.observe(this, Observer<List<Country>> {
+            setupView(it)
+        })
+
     }
+
+
+    fun setupView(list: List<Country>) {
+        val layout = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layout
+        recyclerView.adapter = HomeAdapter(list)
+    }
+
+
 }
---
+++ app/src/main/java/iti/intake40/covidtracker/view/adapters/HomeAdapter.kt
@@ -0,0 +1,29 @@
+package iti.intake40.covidtracker.view.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import iti.intake40.covidtracker.R
+import iti.intake40.covidtracker.model.Country
+import kotlinx.android.synthetic.main.card_country.view.*
+
+class HomeAdapter (private val list: List<Country>): RecyclerView.Adapter<HomeAdapter.VH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val inflate = LayoutInflater.from(parent.context).inflate(R.layout.card_country,parent,false)
+        return VH(inflate)  //as VH
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.itemView.card_county_name.text = list[position].countryName
+        holder.itemView.card_total_cases.text = list[position].cases
+        holder.itemView.card_total_coverd.text = list[position].totalRecovered
+        holder.itemView.card_total_deaths.text = list[position].deaths
+    }
+
+
+    class VH(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
