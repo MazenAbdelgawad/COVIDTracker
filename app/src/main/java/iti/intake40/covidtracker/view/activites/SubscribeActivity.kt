8cc106cfe61a619b931bbf0fcb2eb70a949d0d2b
ahmedsaeed <ahmed.saeed.rashed.1001@gmail.com>
2020-04-16 02:27:08 +0200

    show information about subscribe Activity

---
+++ app/src/main/java/iti/intake40/covidtracker/view/activites/SubscribeActivity.kt
@@ -0,0 +1,13 @@
+package iti.intake40.covidtracker.view.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import iti.intake40.covidtracker.R
+
+class SubscribeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subscribe)
+    }
+}
